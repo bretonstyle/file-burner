3ac2ed874f022d3ce5dc06860262dc206368424c
Matt Breton <breton16@github.com>
2024-03-08 16:12:56 -0500

    more

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def test_openai():
     response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "user", "content": "Write me a Haiku in the style of a pirate."},
+        {"role": "user", "content": "Write me a Haiku, in the form of a pirate"},
     ],
     temperature=0,
     )
